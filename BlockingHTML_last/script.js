2dfe618b282094360ed86d4ff728b7555ad52221
川島悠太 <45031867+Yuta-Kawashima@users.noreply.github.com>
2020-01-18 05:05:26 +0900

    add message.

--- BlockingHTML_last/script.js
+++ BlockingHTML_last/script.js
@@ -1,50 +1,15 @@
 $(function(){
-    var $deferredAnim = $.Deferred( function( deferredAnim ){
- 
-        //thenでファンクション名を繋げる  
-        deferredAnim.then(anim_01)
-        .then(anim_02)
-        .then(anim_03)     
-      });
-       
-      //実行
-      $deferredAnim.resolve(); 
-
-      function anim_01(){
-        $('#reading_one').fadeIn(2000);
-        $('#prodramming_one').fadeIn(2000);
-      }
-      function anim_02(){
-        $('#reading_two').fadeIn(2000);
-        $('#prodramming_two').fadeIn(2000);
-      }
-      function anim_03(){
-        $('#reading_three').fadeIn(2000);
-        $('#prodramming_three').fadeIn(2000);
-      }
-
-    /*$('.content').each(function(e,v){
-        $(this).fadeIn(2000);
-    });
+    $('.content').fadeIn(2000);
     
-    setTimeout(function(){
-        $('#reading_one').fadeIn(2000);
-    },500);
-    setTimeout(function(){
-        $('#reading_two').fadeIn(2000);
-    },500);
-    setTimeout(function(){
-        $('#reading_three').fadeIn(2000);
-    },500);
+    // hoverInの処理
+    var handleHoverIn = function() {
+        $(this).css("border","solid 5px #305097");
+    }
+    // hoverOutの処理
+    var handleHoverOut = function() {
+        $(this).css("border","solid 0px #1dc1d6");   
+    }
+    // Hoverイベントを設定する.
+    $('.content').hover(handleHoverIn, handleHoverOut);
 
-   $(docment).ready(fucntion(){
-    $.when(
-     $('#reading_one').fadeIn(2000);
-     $('#reading_two').fadeIn(2000);
-     $('#reading_three').fadeIn(2000);
-    ).done(function(){
-        $('prodramming').fadeIn(2000)
-    });
-   });
-   */
 })
